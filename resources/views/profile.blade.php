85c6aaecf0c1ec2860cf92932e14d20a2f9de8ff
kilyfa <122576090+kilyfa@users.noreply.github.com>
2024-06-26 00:16:06 +0700

    bug

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -39,24 +39,24 @@
                 <form class=" space-y-6" action="{{ route('profile.update-profile') }}" method="POST">
                     @csrf
                     <div class="flex flex-col">
-                        <label for="name" class="text-sm font-medium text-gray-700">Name</label>
-                        <input type="text" value="{{ Auth::user()->name }}" id="name" name="name" class="text-gray-900 mt-1 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                        <label for="name" class="text-sm font-medium text-gray-700 ">Name</label>
+                        <input type="text" value="{{ Auth::user()->name }}" id="name" name="name" class="bg-white text-gray-900 mt-1 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                     </div>
                     <div class="flex flex-col">
-                        <label for="email" class="text-sm font-medium text-gray-700">Email</label>
-                        <input type="email" value="{{ Auth::user()->email }}" id="email" name="email" class="text-gray-900 mt-1 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                        <label for="email" class="text-sm font-medium text-gray-700 ">Email</label>
+                        <input type="email" value="{{ Auth::user()->email }}" id="email" name="email" class="bg-white text-gray-900 mt-1 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                     </div>
                     <div class="flex flex-col">
-                        <label for="current-password" class="text-sm font-medium text-gray-700">Current Password</label>
-                        <input type="password" id="current-password" name="current_password" class="text-gray-900 mt-1 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                        <label for="current-password" class="text-sm font-medium text-gray-700 ">Current Password</label>
+                        <input type="password" id="current-password" name="current_password" class="bg-white text-gray-900 mt-1 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                     </div>
                     <div class="flex flex-col">
-                        <label for="new-password" class="text-sm font-medium text-gray-700">New Password</label>
-                        <input type="password" id="new-password" name="new_password" class="text-gray-900 mt-1 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                        <label for="new-password" class="text-sm font-medium text-gray-700 ">New Password</label>
+                        <input type="password" id="new-password" name="new_password" class="bg-white text-gray-900 mt-1 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                     </div>
                     <div class="flex flex-col">
-                        <label for="confirm-password" class="text-sm font-medium text-gray-700">Confirm New Password</label>
-                        <input type="password" id="confirm-password" name="new_password_confirmation" class="text-gray-900 mt-1 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                        <label for="confirm-password" class="text-sm font-medium text-gray-700 ">Confirm New Password</label>
+                        <input type="password" id="confirm-password" name="new_password_confirmation" class="bg-white text-gray-900 mt-1 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                     </div>
                     <div>
                         <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
